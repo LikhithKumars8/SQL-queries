77d50bb6b962bfd6ba01321fe4c51d3ee001000e
Likhith Kumar S <102742462+LikhithKumars8@users.noreply.github.com>
2024-11-13 11:28:08 +0530

    Create leetcode_practice.sql

---
+++ leetcode_practice.sql
@@ -0,0 +1,30 @@
+1. Table: Products
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key (column with unique values) for this table.
+low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+Write a solution to find the ids of products that are both low fat and recyclable.
+
+SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable='Y';
+
+2. Table: Customer
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+Find the names of the customer that are not referred by the customer with id = 2.
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2 or referee_id is null;
