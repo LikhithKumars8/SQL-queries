894535b0097d6002b99fd43ad01d1f7cac7e9f13
Likhith Kumar S <102742462+LikhithKumars8@users.noreply.github.com>
2024-11-13 16:19:52 +0530

    Update leetcode_practice.sql

--- leetcode_practice.sql
+++ leetcode_practice.sql
@@ -28,3 +28,24 @@ Find the names of the customer that are not referred by the customer with id = 2
 SELECT name
 FROM Customer
 WHERE referee_id != 2 or referee_id is null;
+
+3. Table: World
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+A country is big if:
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
